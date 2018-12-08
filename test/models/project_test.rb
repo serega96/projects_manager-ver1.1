acff1c2b5019c69044d2747bc3767b0f1207983d
serega96 <5875030@gmail.com>
2018-12-08 20:16:33 +0300

    Added firs test to 'Project'

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -4,4 +4,8 @@ class ProjectTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should not save project without information" do
+    project = Project.new
+    assert_not project.save
+  end
 end
