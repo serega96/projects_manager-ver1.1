71ad1f4fe36522fae73bf2d3ebf960ca3ea19ea7
serega96 <5875030@gmail.com>
2018-12-09 19:00:27 +0300

    Added tests to task

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save task without information" do
+    task = Task.new
+    assert_not task.save
+  end
+
+  test "save task with information and destroy for id" do
+    task = Task.new(id: '1', title: "Test", description: 'TEst description', status: 1, project_id: 298486374)
+    assert task.save
+
+    task_d = Task.destroy(1)
+    assert task_d.destroy
+  end
 end
