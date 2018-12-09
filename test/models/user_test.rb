697a2bc8d3fde8e138f319e0eed183316588e38f
serega96 <5875030@gmail.com>
2018-12-09 19:01:01 +0300

    Added tests to user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save user without information" do
+    user = User.new
+    assert_not user.save
+  end
+
+  test "save user with information and destroy for id" do
+    user = User.new(id: '1',
+                    email: 'test4@test.by',
+                    password: '123456',
+                    password_confirmation: '123456',
+                    admin: true, first_name: 'test4')
+    assert user.save
+
+    user_d = User.destroy(1)
+    assert user_d.destroy
+  end
 end
